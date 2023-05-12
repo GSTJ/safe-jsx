d86a81054d8974970358b29113658a577a7d5725
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-05-12 00:48:09 -0300

    feat: make it faster with early returns ⚡️

--- src/rules/jsx-explicit-boolean.js
+++ src/rules/jsx-explicit-boolean.js
@@ -12,37 +12,35 @@ module.exports = {
     return {
       LogicalExpression(node) {
         // We're only interested in && operators
-        if (node.operator !== '&&') {
-          return;
-        }
+        if (node.operator !== '&&') return;
 
         // We're only interested in JSX elements on the right-hand side
-        if (node.right.type !== 'JSXElement') {
-          return;
-        }
+        if (node.right.type !== 'JSXElement') return;
 
         const { left } = node;
+
         const isExplicitBooleanConversion = left.type === 'CallExpression'
           && left.callee.type === 'Identifier'
           && left.callee.name === 'Boolean';
+        if (isExplicitBooleanConversion) return;
 
         const isBooleanLiteral = left.type === 'Literal' && typeof left.value === 'boolean';
+        if (isBooleanLiteral) return;
 
         const isBooleanVariable = left.type === 'Identifier'
           && context.getScope().variables.some((variable) => variable.name === left.name && variable.defs.some((def) => def.type === 'Variable' && typeof def.node.init.value === 'boolean'));
+        if (isBooleanVariable) return;
 
-        if (!isExplicitBooleanConversion && !isBooleanLiteral && !isBooleanVariable) {
-          context.report({
-            node,
-            messageId: 'booleanConversion',
-            fix(fixer) {
-              return fixer.replaceTextRange(
-                [left.range[0], left.range[1]],
-                `Boolean(${context.getSourceCode().getText(left)})`,
-              );
-            },
-          });
-        }
+        context.report({
+          node,
+          messageId: 'booleanConversion',
+          fix(fixer) {
+            return fixer.replaceTextRange(
+              [left.range[0], left.range[1]],
+              `Boolean(${context.getSourceCode().getText(left)})`,
+            );
+          },
+        });
       },
     };
   },
